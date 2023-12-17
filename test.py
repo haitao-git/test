02124f29cac9f669ecb2caa7a636b4556c2e1a9e
haitao-git <haitaohuang@hotmail.com>
2023-12-17 17:03:17 -0500

    hooks

---
+++ test.py
@@ -0,0 +1,22 @@
+# begin of the code
+
+
+# start of the normal block
+if true:
+    print("this is the begin of the normal code block")
+else:
+    print("this is the end of the normal code block")
+# end of the normal block
+
+# some other code
+print("some other code:)
+
+# start of the normal block
+if true:
+    print("this is the begin of the normal code block")
+else:
+    print("this is the end of the normal code block")
+# end of the normal block
+
+
+# the end of the code
